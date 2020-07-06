37cb56bc8050acb178e318761a9505a6b6aaba6e
Abraham Yang <abraham.yang@publicissapient.com>
2020-07-06 13:24:13 +0800

    /

--- src/components/herobanner.jsx
+++ src/components/herobanner.jsx
@@ -4,6 +4,10 @@ import Image from 'material-ui-image';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
 
 import heroImage from '../assets/images/herobanner/hero-image.png';
 import '../assets/css/sass/herobanner.scss';
@@ -21,15 +25,65 @@ const useStyles = makeStyles((theme) => ({
 
 const HeroBanner = (props) => {
   const classes = useStyles();
+
+  const [open, setOpenState] = React.useState(false);
+
+  const handleLinkOpen = () => {
+    setOpenState(true);
+  };
+
+  const handleLinkClose = () => {
+    setOpenState(false);
+  };
+
   return (
     <Box id='home' className={classes.root}>
       <Grid className='hero-container' container alignItems='center'>
         <Grid item xs={1} />
         <Grid className='hero-text' item xs={5}>
           <Typography variant='h1' noWrap>
-            Looking to the Future
+            Microsoft 365 Challenge
           </Typography>
-          <p>Microsoft 365 Challenge</p>
+          <p>
+            Let your imagination run wild and create a piece of artwork relating
+            to the theme of “Looking to the Future” and stand to win a chance to
+            win a brand new HONOR MagicBook 14.
+          </p>
+
+          <Button onClick={handleLinkOpen} className='herobanner-button'>
+            Explore MagicBook 14 {'>'}
+          </Button>
+          <Dialog
+            open={open}
+            onClose={handleLinkClose}
+            className='herobanner-dialog'
+          >
+            <DialogContent>
+              <DialogContentText>
+                <Button
+                  onClick={handleLinkClose}
+                  href='//www.hihonor.com/france/product/honor-magicbook-14'
+                  className='herobanner-button'
+                >
+                  France - Français {'>'}
+                </Button>
+                <Button
+                  onClick={handleLinkClose}
+                  href='//www.hihonor.com/germany/product/honor-magicbook-14-365'
+                  className='herobanner-button'
+                >
+                  Germany - Deutsch {'>'}
+                </Button>
+                <Button
+                  onClick={handleLinkClose}
+                  href='//www.hihonor.com/unitedkingdom/product/honor-magicbook-14'
+                  className='herobanner-button'
+                >
+                  United Kingdom - English {'>'}
+                </Button>
+              </DialogContentText>
+            </DialogContent>
+          </Dialog>
         </Grid>
         <Grid className='hero-image' item xs={6}>
           <Image src={heroImage} aspectRatio={1832 / 1090}></Image>
--- src/components/tutorial.jsx
+++ src/components/tutorial.jsx
@@ -14,6 +14,7 @@ import DialogContent from '@material-ui/core/DialogContent';
 import TitleBullets from '../utils/TitleBullets';
 
 import video01 from '../assets/images/stillonoir_magicbook_process-video_music.mp4';
+import video02 from '../assets/images/0630_honor 5_2.mp4';
 
 import 'swiper/css/swiper.css';
 
@@ -23,6 +24,8 @@ import bgImage from '../assets/images/judge/judge_bg.png';
 import avatar01 from '../assets/images/judge/Monsieur.png';
 import avatar02 from '../assets/images/judge/Sebastiano.png';
 import avatar03 from '../assets/images/judge/Rubens.png';
+import videoThumb01 from '../assets/images/tutorial/video_thumb_01.jpg';
+import videoThumb02 from '../assets/images/tutorial/video_thumb_02.jpg';
 
 const judges = [
   {
@@ -30,13 +33,23 @@ const judges = [
     description:
       'French tech content creator. He publishes many videos on his YouTube channel presenting new products. But he also talks about our uses and advises us in our technological purchases.',
     avatar: avatar01,
+    videoThumb: videoThumb01,
     videoPath: video01,
+  },
+  {
+    name: 'Monsieur',
+    description:
+      'French tech content creator. He publishes many videos on his YouTube channel presenting new products. But he also talks about our uses and advises us in our technological purchases.',
+    avatar: avatar01,
+    videoThumb: videoThumb02,
+    videoPath: video02,
   },
 ];
 
 const Tutorial = (props) => {
   const [popupVideoPath, setPoupVideoPath] = React.useState('');
   const [open, setOpen] = React.useState(false);
+
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -52,10 +65,9 @@ const Tutorial = (props) => {
     speed: 1000,
     autoplay: false,
     spaceBetween: 0,
-    slideToClickedSlide: true,
     centeredSlides: true,
-    centeredSlidesBounds: true,
-    watchOverflow: true,
+    centeredSlidesBounds: false,
+    loop: false,
   };
 
   return (
@@ -66,13 +78,13 @@ const Tutorial = (props) => {
         </Typography>
         <TitleBullets />
         <Swiper {...params}>
-          {judges.map(({ name, description, videoPath }) => (
+          {judges.map(({ name, description, videoPath, videoThumb }) => (
             <div key={name}>
               <div className='swiper-card-container'>
                 <div className='swiper-card-inner'>
-                  <Card className='card-container'>
-                    <CardContent className='card-video-thumb'>
-                      <video src={`${videoPath}#t=0,1`}></video>
+                  <div className='card-container'>
+                    <div className='card-video-thumb'>
+                      <img src={videoThumb} alt='' />
                       <IconButton
                         onClick={() => {
                           setPoupVideoPath(videoPath);
@@ -82,16 +94,13 @@ const Tutorial = (props) => {
                       >
                         <PlayArrowIcon />
                       </IconButton>
-                    </CardContent>
-                    {/* <CardContent className='card-content'>
-                      <Typography variant='h3'>{name}</Typography>
-                      <Typography variant='h5'>{description}</Typography>
-                    </CardContent> */}
-                  </Card>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
           ))}
+
         </Swiper>
 
         <Dialog className='video-popup' open={open} onClose={handleClose}>
